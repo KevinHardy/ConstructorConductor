7f5dd63f86c34bbb86ede24746e8b3e0133749ba
Kevin Hardy <hardy95k@yahoo.com>
2015-01-28 21:03:35 -0700

    updated arrayProperty.js instanceArray.js quizApp.js sayName.js

--- quizApp.js
+++ quizApp.js
@@ -6,27 +6,38 @@
 //Create a User constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
   //code here
-
+var User = function(name, email, password, totalScore) {
+	this.name = name;
+	this.email = email;
+	this.password = password;
+	this.totalScore = totalScore;
+}
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
   //code here
-
+var Question = function(title, answersArray, rightAnswer, difficulty) {
+	this.title = title;
+	this.answersArray = answersArray;
+	this.rightAnswer = rightAnswer;
+	this.difficulty = difficulty;
+}
 
 //Create a users Array which is going to hold all of our users.
 
   //code here
-
+var usersArr = [];
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
   //code here
+usersArr.push(new User('Kevin', 'hardy95k@yahoo.com', 'password123', 95), new User('Kelly', 'hotwriter99@yahoo.com', 'password456', 100), new User('Tychus', 'Tychus@gmail.com', 'password789', 97));
 
 
 //Create a questions Array which is going to hold all of our questions
 
   //code here
-
+var QuestionsArr = [];
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
 //title: 'T/F: Inheritance is achieved in JavaScript through Prototypes?'
@@ -35,14 +46,22 @@
 //Fill in the rest of the required data as you see appropriate.
 
   //code here
-
+var question1 = new Question('T/F: Inheritance is achieved in JavaScript through Prototypes?', ansArr = [], 'T', 'easy');
+var question2 = new Question('T/F: JavaScript is just a scripting version of Java', ansArr = [], 'F', 'easy');
+var question3 = new Question('T/F: In Javascript, == doesn\'t check \'type\' but just the value - where === checks type and value', ansArr = [], 'T', 'Hard');
 
 //Now push all of your instances of Question into the questions Array
 
   //code here
+QuestionsArr.push(question1, question2, question3);
 
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
   //code here
-
+for (var i = 0; i < usersArr.length; i++) {
+	console.log(usersArr[i]);
+}
+for (var i = 0; i < QuestionsArr.length; i++) {
+	console.log(QuestionsArr[i]);
+}
